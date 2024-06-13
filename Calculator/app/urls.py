0a88d8edcdc83658069e10e3374a312c11cc1183
balubbr <dwarampudibalajireddy@gmail.com>
2024-06-13 13:05:37 +0530

    updated the changes

--- Calculator/app/urls.py
+++ Calculator/app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import NumbersView
 
 urlpatterns = [
-    path('numbers/<str:numberid>/', Calc.as_view(), name='numbers')
+    path('numbers/<str:number_type>/', Calc.as_view(), name='numbers')
 ]
--- Calculator/app/views.py
+++ Calculator/app/views.py
@@ -4,21 +4,7 @@ from rest_framework.views import APIView
 from django.utils.decorators import method_decorator
 from collections import deque
 import time
-import logging
 
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-
-
-
-windows = {
-    'p': deque(maxlen=10),
-    'f': deque(maxlen=10),
-    'e': deque(maxlen=10),
-    'r': deque(maxlen=10)
-}
 
 TEST_SERVER={
     'p': 'http://20.244.56.144/test/primes',
@@ -27,12 +13,19 @@ TEST_SERVER={
     'r': 'http://20.244.56.144/test/rand',
 }
 
+windows = {
+    'p': deque(maxlen=10),
+    'f': deque(maxlen=10),
+    'e': deque(maxlen=10),
+    'r': deque(maxlen=10)
+}
+
 BEARER_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE4MjYyNjI2LCJpYXQiOjE3MTgyNjIzMjYsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjdlODM5YzIwLTJjZmItNDNiZS05Nzg3LTg0NDZjNzdiYjFjZSIsInN1YiI6ImR3YXJhbXB1ZGliYWxhamlyZWRkeUBnbWFpbC5jb20ifSwiY29tcGFueU5hbWUiOiJLTCBVTklWRVJTSVRZIiwiY2xpZW50SUQiOiI3ZTgzOWMyMC0yY2ZiLTQzYmUtOTc4Ny04NDQ2Yzc3YmIxY2UiLCJjbGllbnRTZWNyZXQiOiJJWEdxZlVpd2Rpa2FsbU5wIiwib3duZXJOYW1lIjoiRFdBUkFNUFVESSBCQUxBSkkgUkVERFkiLCJvd25lckVtYWlsIjoiZHdhcmFtcHVkaWJhbGFqaXJlZGR5QGdtYWlsLmNvbSIsInJvbGxObyI6IjIxMDAwMzAxNDAifQ.sOcTU1pN9ulvNO-SsSogR4uDnzbfhSsv97yKPAwu5RM'
 
 
 class Calc(APIView):
     def get(self, request, number_type):
-        if numberid not in TEST_SERVER:
+        if number_type not in TEST_SERVER:
             return JsonResponse({'error': 'Invalid number type'}, status=400)
 
         start_time = time.time()
@@ -56,11 +49,11 @@ class Calc(APIView):
         response_data = {
             "numbers": fetched,
             "windowPrevState": prev,
-            "windowCurrState": list(windows[numberid]),
+            "windowCurrState": list(windows[number_type]),
             "avg": sum(curr_window_state) / len(curr_window_state) if curr_window_state else 0
         }
 
-        if time.time() - start_time > 0.5:
+        if time.time()-start_time > 0.5:
             return JsonResponse({'error': 'Response time exceeded 500 milli Seconds'}, status=500)
 
         return JsonResponse(response_data)
